388611cefe88e0ca1a7949d1e91ee46782fb4bc4
stareta1202 <stareta1202@gmail.com>
2020-11-22 01:45:41 +0900

    libft first

--- libft/true libft/ft_lstmap.c
+++ libft/true libft/ft_lstmap.c
@@ -1,2 +1,27 @@
 #include "libft.h"
 
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list *node;
+    t_list *curr;
+    t_list *temp;
+
+    if (lst == 0 || f == 0)
+        return (NULL);
+    if((node = ft_lstnew(f(lst->content))) == 0)
+        return (NULL);
+    curr = node;
+    lst = lst->next;
+    while(lst)
+    {
+        if ((temp = ft_lstnew(f(lst->content))) == 0)
+        {
+            ft_lstclear(&node, del);
+            return (NULL);
+        }
+        curr->next = temp;
+        curr = temp;
+        lst = lst->next;
+    }
+    return (node);
+}
